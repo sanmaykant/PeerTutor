11711d4e548c522aeae5e029f990a810584ff5ce
sanmay <sanmaykant1@gmail.com>
2025-06-19 11:34:39 +0530

    Configured shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   define: {
     global: {}
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
