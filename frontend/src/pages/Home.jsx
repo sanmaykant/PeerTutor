14d4703d731ba9c7fb070e7477084691831e6186
sanmay <sanmaykant1@gmail.com>
2025-06-22 22:51:31 +0530

    Home prototype done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -28,6 +28,20 @@ export default function Home() {
                 </div>
             </div>
         </header>
+
+        <main>
+            <div className="w-full h-[50vh] max-w-150 mx-auto text-center flex flex-col justify-center">
+                <span className="font-open-sans text-[32px] font-semibold text-generic-green">Turning Gaps Into Gains</span>
+                <span className="font-open-sans text-[32px] font-semibold mb-4 text-generic-green">Together</span>
+                <p className="mb-6 text-[16px] text-generic-green font-lato">
+                    Whether you're struggling in computer networks but great at database management, or a master in software engineering but need help with operating systems, there's someone out there who can help — and someone who needs you. Get matched with the right peer, and connect and grow through mutual support.
+                </p>
+                <Button className="w-36 mx-auto" variant="navPrimary" size="nav" onClick={() => navigate("/signup")}>
+                    Explore For Free
+                </Button>
+            </div>
+            <div className="h-[50vh] w-full bg-gray-300" />
+        </main>
         </>
     );
 }
