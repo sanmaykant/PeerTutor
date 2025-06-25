674c03a7fa64cd6130940f14deac33e762e455c9
Shravani <shravanispathak@gmail.com>
2025-06-25 13:02:17 +0530

    Added hero section to About page

--- frontend/src/components/ui/accordion.jsx
+++ frontend/src/components/ui/accordion.jsx
@@ -17,7 +17,7 @@ function AccordionItem({
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-b last:border-b-0", className)}
+      className={cn("border-[0.5px] border-opacity-80 mb-[18px] px-[18px]", className)}
       {...props} />
   );
 }
@@ -32,7 +32,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left font-hanken-grotesque font-medium text-[18px] text-[#103E40] transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
           className
         )}
         {...props}>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,53 @@
 import Navbar from "../components/Navbar";
+import lastIconAboutPage from "../../public/lastIconAboutPage.svg"
+import firstIconAboutPage from "../../public/firstIconAboutPage.svg"
+import secondIconAboutPage from "../../public/secondIconAboutPage.svg"
+import about_hero_image from "../../public/about_hero_image.svg"
 import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { AccordionDemo } from "../components/CustomAccordian";
 
 function About() {
   return (
     <>
-    <Navbar />
-    
+    <Navbar />  
+
+    <div className="w-full h-[calc(100vh-74px)] flex justify-center items-center ">
+        <div className="flex justify-center items-center gap-[80.12px]">
+            <div className="flex flex-col gap-[32px]">
+                <div className="font-hanken-grotesque font-bold text-[32px] text-[#003B44]">
+                    <span>We're building a better way to </span><br></br>
+                    <span> learn — Peer to Peer</span>
+                </div>
+                <div className="w-[470px]">
+                    <AccordionDemo />
+                </div>
+            </div>
+            <img src={about_hero_image} alt="hero image" />
+        </div>
+    </div>
+
+    <div className="flex justify-center items-center gap-[330px]">
+        <div className=" flex flex-col items-center justify-center text-center">
+            <img src={firstIconAboutPage} alt="handshake icon" />
+            <span>Mutual Matching : Based <br></br> on complementary <br></br> strengths and weaknesses</span>
+        </div>
+
+        <div>
+            <img src={secondIconAboutPage} alt="book icon" />
+            <span></span>
+        </div>
+
+        <div>
+            <img src={lastIconAboutPage} alt="chat icon" />
+            <span></span>
+        </div>
+    </div>
     </>
   )
 }
