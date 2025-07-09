6d116be762a8fdadad96dc1487aece156c8bfeb2
sanmay <sanmaykant1@gmail.com>
2025-07-09 14:39:58 +0530

    Allowed auth_token header

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
   origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"],
+  allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "auth_token"],
 };
 
 app.use(cors(corsOptions));
